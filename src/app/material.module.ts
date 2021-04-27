b1244b1fe249d4cdc359b843f5e8894089c34ccf
Damian Opacki <damian.opacki@gmail.com>
2021-04-27 11:06:46 +0200

    init

---
+++ src/app/material.module.ts
@@ -0,0 +1,19 @@
+import {NgModule} from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
+@NgModule({
+  exports: [
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatIconModule,
+    MatInputModule,
+    MatToolbarModule
+  ]
+})
+export class MaterialModule {}
